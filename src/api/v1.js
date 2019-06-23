60a0f442aa90dfdc386e666e23d130c2f9f603e8
Tia Rose <shortyshort789@aol.com>
2019-06-22 22:29:30 -0700

    got swagger to work

--- src/api/v1.js
+++ src/api/v1.js
@@ -2,7 +2,7 @@
 
 /**
  * API Router Module (V1)
- * Integrates with various models through a common Interface (.get(), .post(), .put(), .delete())
+ * Integrates with v1rious models through a common Interface (.get(), .post(), .put(), .delete())
  * @module src/api/v1
  */
 
@@ -18,24 +18,19 @@ const router = express.Router();
 // Evaluate the model, dynamically
 router.param('model', modelFinder);
 
-
 // API Routes
-
 /**
  * Get a list of records for model provided
- * @method GET
- * @route /{model}
+ * @route GET /{model}
  * @param {string} model.path.required - Resource model name
  * @returns {Object} 500 - Server error
  * @returns {Object} 200 - { count: 2, results: [{}, {}]}
  */
 router.get('/api/v1/:model', handleGetAll);
 
-
 /**
  * Creates a list of records for model provided
- * @method POST
- * @route /{model}
+ * @route POST /{model}
  * @param {string} model.path.required - Resource model name
  * @consumes application/json application/xml
  * @returns {Object} 500 - Server error
@@ -43,54 +38,45 @@ router.get('/api/v1/:model', handleGetAll);
  */
 router.post('/api/v1/:model', auth('create'), handlePost);
 
-
 /**
- * Get a record for model id provided
- * @method GET
- * @route /{model}/{id}
+ * Get a list of records for model id provided
+ * @route GET /{model}/{id}
  * @param {string} model.path.required - Resource model name
  * @param {number} id.path.required - Resource model name
  * @returns {Object} 500 - Server error
- * @returns {Object} 200 - { count: 1, results: [ {} ]}
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
  */
 router.get('/api/v1/:model/:id', handleGetOne);
 
-
 /**
- * Modifies a record for model provided
- * @method PUT
- * @route /{model}/{id}
+ * Modifies of records for model provided
+ * @route PUT /{model}/{id}
  * @param {string} model.path.required - Resource model name
  * @param {number} id.path.required - Resource model name
- * @consumes application/json application/xml
  * @returns {Object} 500 - Server error
- * @returns {Object} 200 - { count: 1, results: [ {} ]}
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
  */
-
-router.put('/api/v1/:model/:id',auth('update'), handlePut);
+router.put('/api/v1/:model/:id', auth('update'), handlePut);
 
 /**
- * Modifies a record for model provided
- * @method PATCH
- * @route /{model}/{id}
+ * Modifies of records for model provided
+ * @route patch /{model}/{id}
  * @param {string} model.path.required - Resource model name
  * @param {number} id.path.required - Resource model name
- * @consumes application/json application/xml
  * @returns {Object} 500 - Server error
- * @returns {Object} 200 - { count: 1, results: [ {} ]}
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
  */
-router.patch('/api/v1/:model/:id',auth('update'), handlePut);
-
+router.patch('/api/v1/:model/:id', auth('update'), handlePut);
 
 /**
  * Deletes records for model provided
- * @method DELETE
- * @route /{model}/{id}
+ * @route DELETE /{model}/{id}
  * @param {string} model.path.required - Resource model name
  * @param {number} id.path.required - Resource model name
  * @returns {Object} 500 - Server error
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
  */
-router.delete('/api/v1/:model/:id',auth('delete'), handleDelete);
+router.delete('/api/v1/:model/:id', auth('delete'), handleDelete);
 
 // Route Handlers
 
@@ -98,10 +84,9 @@ router.delete('/api/v1/:model/:id',auth('delete'), handleDelete);
    * @function handleGetAll
    * @param {object} request - request object
    * @param {object} response - response object
-   * @param {function} next - calls next middleware
+   * @param {function} next - next function which calls next middleware
    * @desc Middleware that handles get all route call
    */
-
 function handleGetAll(request,response,next) {
   request.model.get()
     .then( data => {
@@ -118,10 +103,9 @@ function handleGetAll(request,response,next) {
    * @function handleGetOne
    * @param {object} request - request object
    * @param {object} response - response object
-   * @param {function} next - calls next middleware
-   * @desc Middleware that handles get one call
+   * @param {function} next - next function which calls next middleware
+   * @desc Middleware that handles get one route call
    */
-
 function handleGetOne(request,response,next) {
   request.model.get(request.params.id)
     .then( result => response.status(200).json(result[0]) )
@@ -132,10 +116,9 @@ function handleGetOne(request,response,next) {
    * @function handlePost
    * @param {object} request - request object
    * @param {object} response - response object
-   * @param {function} next - calls next middleware
+   * @param {function} next - next function which calls next middleware
    * @desc Middleware that handles post route
    */
-
 function handlePost(request,response,next) {
   request.model.post(request.body)
     .then( result => response.status(200).json(result) )
@@ -146,10 +129,9 @@ function handlePost(request,response,next) {
    * @function handlePut
    * @param {object} request - request object
    * @param {object} response - response object
-   * @param {function} next - calls next middleware
+   * @param {function} next - next function which calls next middleware
    * @desc Middleware that handles put route
    */
-
 function handlePut(request,response,next) {
   request.model.put(request.params.id, request.body)
     .then( result => response.status(200).json(result) )
@@ -157,22 +139,16 @@ function handlePut(request,response,next) {
 }
 
 /**
-   * @function handleDelete
+   * @function handlePut
    * @param {object} request - request object
    * @param {object} response - response object
-   * @param {function} next - calls next middleware
+   * @param {function} next - next function which calls next middleware
    * @desc Middleware that handles delete route
    */
-
 function handleDelete(request,response,next) {
   request.model.delete(request.params.id)
     .then( result => response.status(200).json(result) )
     .catch( next );
 }
-
-/**
- * Export object
- * @type {Object}
- */
 
 module.exports = router;
