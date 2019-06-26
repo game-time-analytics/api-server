c66843a8aa1e868a5344daf175274e6ef7eb47ec
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-26 12:10:28 -0700

    404 and 500, Felipe, Tia, Jeff

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -1,11 +1,20 @@
 'use strict';
 
 /**
- * @module 404
- * @param {object} req - the request object
- * @param {object} res - the response object
- * @desc client error  
- */
+* @module src/middleware/404
+*/
+
+/**
+* @method 404
+* @param {object} req - the request object
+* @param {object} res - the response object
+* @param {Function} next - next function which calls next middleware
+* @returns {string} 404 client error
+* Export object
+* @type {Object}
+* @desc client error function handler
+*/
+
 module.exports = (req,res,next) => {
   let error = { error: 'Resource Not Found' };
   res.status(404).json(error).end();
--- src/middleware/500.js
+++ src/middleware/500.js
@@ -1,12 +1,21 @@
 'use strict';
 
 /**
- * @module 500
- * @param {object} err - the error object
- * @param {object} req - request object
- * @param {object} res - response object
- * @desc server error
- */
+* @module src/middleware/500
+*/
+
+/**
+* @method 500
+* @param {object} err - the error object
+* @param {object} req - request object
+* @param {object} res - response object
+* @param {Function} next - next function which calls next middleware
+* @returns {string} 500 server error
+* Export object
+* @type {Object}
+* @desc server error handler function
+*/
+
 module.exports = (err, req, res, next) => {
   let error = { error: err };
   res.status(500).json(error).end();
